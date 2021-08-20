24bd0a265f163b6977dca590add68ca9f75f15a1
knyshman <88670748+knyshman@users.noreply.github.com>
2021-08-20 21:24:42 +0300

    update requirements

--- crm/settings.py
+++ crm/settings.py
@@ -82,12 +82,15 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -139,3 +142,7 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 2621440
 FILE_UPLOAD_PERMISSION = 0o700
 CKEDITOR_UPLOAD_PATH = os.path.join(BASE_DIR, 'archives')
 DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+
+
+import django_heroku
+django_heroku.settings(locals())
